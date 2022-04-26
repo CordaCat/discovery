e1ef57dd1123a0bf104e08fc95fb4d4de61e04ee
CordaCat <jalalhannan0@gmail.com>
2022-04-26 02:56:34 +0100

    cleanup and tests

--- test/test.js
+++ test/test.js
@@ -15,7 +15,7 @@ beforeEach(async () => {
     { value: 1000 }
   );
 
-  // We set game as the variable name for our game object
+  // We set the variables name for tests
   game = await contract.games([0]);
   potAmount = await game[2];
   host = await game[0];
@@ -49,11 +49,11 @@ describe("Join a game", function () {
         { value: 1000 }
       );
   });
-  it("Should increase guest bet amount by entry fee", async function () {
+  it("Should increase the pot amount by the entry fee amount", async function () {
     let game = await contract.games([0]);
-    expect(game[2]).to.equal(2000);
+    let new_pot = await game[2];
+    expect(new_pot).to.equal(2000);
   });
-
   it("Should change game status to STATUS_IN_PROGESS", async function () {
     let game = await contract.games([0]);
     expect(game[3]).to.equal(1);
@@ -62,29 +62,27 @@ describe("Join a game", function () {
 
 describe("Reveal a move", function () {
   beforeEach(async () => {
-    // We join the existing game (game[0]) with Alice's account
+    // We join the existing game (game[0]) with Alice's account, move is scissors password is "pass"
     await contract
       .connect(alice)
       .joinGame(
         0,
-        "0xc5df0b6cfb09a97697e51c816ef269f0eb180ebf4183ab2688ea7c5a9a2f9ea8",
+        "0x981eba72fe369eaf5013fbafc2cec2f5dfa27ed62ccb51aaa0f9d487fb46a41b",
         { value: 1000 }
       );
   });
   it("Should reveal the host's move", async function () {
     await contract.revealMove(0, 0, "pass");
     let game = await contract.games([0]);
-    console.log[("Game:  ", game)];
-    let player_move = game[0][3];
-    expect(player_move).lessThanOrEqual(2);
+    let host_move = game[0][3];
+    expect(host_move).lessThanOrEqual(2);
   });
 
   it("Should reveal the guest's move", async function () {
-    await contract.revealMove(0, 0, "pass");
+    await contract.revealMove(0, 1, "pass");
     let game = await contract.games([0]);
-    console.log[("Game:  ", game)];
-    let player_move = game[0][3];
-    expect(player_move).lessThanOrEqual(2);
+    let guest_move = game[1][3];
+    expect(guest_move).lessThanOrEqual(2);
   });
 });
 
