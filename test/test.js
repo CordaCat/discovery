b86a07e97687b1f45365664d6bf42c0522bfd9ae
CordaCat <jalalhannan0@gmail.com>
2022-04-26 14:40:15 +0100

    Add mocha

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const { expect } = require("chai");
+const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 
 beforeEach(async () => {
@@ -6,12 +6,15 @@ beforeEach(async () => {
   const RockPaperScissors = await ethers.getContractFactory(
     "RockPaperScissors"
   );
-  // Contract is the concrete instance of our smart contract
+  // contract is the concrete instance of our smart contract, we also specify the entry fee for each game
   contract = await RockPaperScissors.deploy(1000);
+  // We deploy the contract
   await contract.deployed();
   // We create a game instance using the payable createGame function
   await contract.createGame(
+    // Salted Hash of move, in this case the move is Rock and the password is "pass"
     "0xc5df0b6cfb09a97697e51c816ef269f0eb180ebf4183ab2688ea7c5a9a2f9ea8",
+    // We add the entry fee for the game
     { value: 1000 }
   );
 
@@ -50,45 +53,18 @@ describe("Join a game", function () {
       );
   });
   it("Should increase the pot amount by the entry fee amount", async function () {
-    let game = await contract.games([0]);
-    let new_pot = await game[2];
+    const game = await contract.games([0]);
+    const new_pot = await game[2];
     expect(new_pot).to.equal(2000);
   });
   it("Should change game status to STATUS_IN_PROGESS", async function () {
-    let game = await contract.games([0]);
+    const game = await contract.games([0]);
     expect(game[3]).to.equal(1);
   });
 });
 
 describe("Reveal a move", function () {
   beforeEach(async () => {
-    // We join the existing game (game[0]) with Alice's account, move is scissors password is "pass"
-    await contract
-      .connect(alice)
-      .joinGame(
-        0,
-        "0x981eba72fe369eaf5013fbafc2cec2f5dfa27ed62ccb51aaa0f9d487fb46a41b",
-        { value: 1000 }
-      );
-  });
-  it("Should reveal the host's move", async function () {
-    await contract.revealMove(0, 0, "pass");
-    let game = await contract.games([0]);
-    let host_move = game[0][3];
-    expect(host_move).lessThanOrEqual(2);
-  });
-
-  it("Should reveal the guest's move", async function () {
-    await contract.revealMove(0, 1, "pass");
-    let game = await contract.games([0]);
-    let guest_move = game[1][3];
-    expect(guest_move).lessThanOrEqual(2);
-  });
-});
-
-describe("Should Payout winner with pot amount", function () {
-  beforeEach(async () => {
-    // [owner, alice, bob] = await ethers.getSigners();
     // We join the existing game (game[0]) with Alice's account
     await contract
       .connect(alice)
@@ -100,19 +76,49 @@ describe("Should Payout winner with pot amount", function () {
   });
   it("Should reveal the host's move", async function () {
     await contract.revealMove(0, 0, "pass");
-    let game = await contract.games([0]);
-    console.log[("Game:  ", game)];
-    let player_move = game[0][3];
-    expect(player_move).lessThanOrEqual(2);
+    const game = await contract.games([0]);
+    const host_move = game[0][3];
+    expect(host_move).lessThanOrEqual(2);
   });
 
   it("Should reveal the guest's move", async function () {
-    await contract.revealMove(0, 0, "pass");
-    let game = await contract.games([0]);
-    console.log[("Game:  ", game)];
-    let player_move = game[0][3];
-    expect(player_move).lessThanOrEqual(2);
+    await contract.connect(alice).revealMove(0, 0, "pass");
+    const game = await contract.games([0]);
+    const guest_move = game[1][3];
+    expect(guest_move).lessThanOrEqual(2);
   });
+});
+
+describe("Should Payout winner with pot amount", function () {
+  beforeEach(async () => {
+    // We join the existing game (game[0]) with Alice's account
+    await contract
+      .connect(alice)
+      .joinGame(
+        0,
+        "0xc5df0b6cfb09a97697e51c816ef269f0eb180ebf4183ab2688ea7c5a9a2f9ea8",
+        { value: 1000 }
+      );
+  });
+  it("Should reveal the host's move", async function () {
+    await contract.revealMove(0, 0, "pass");
+    const game = await contract.games([0]);
+    const host_move = game[0][3];
+    expect(host_move).lessThanOrEqual(2);
+  });
+
+  it("Should reveal the guest's move", async function () {
+    await contract.connect(alice).revealMove(0, 0, "pass");
+    const game = await contract.games([0]);
+    const guest_move = game[1][3];
+    expect(guest_move).lessThanOrEqual(2);
+  });
+  // it("Should update player status of winner", async function () {});
+
+  // it("Should increase the winners balance by pot amount", async function () {
+  //   assert.fail;
+  // });
+  // it("Should decrease the losers balance by entry fee amount", async function () {});
 });
 
 // Draw
